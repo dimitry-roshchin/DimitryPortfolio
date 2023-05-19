e215f74616f3eb5a5da427a022d59f20ded8b46f
dimitry <dimitry.roshchin@outlook.com>
2023-05-19 12:39:32 -0500

    border changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -70,13 +70,13 @@ every project I work on. I look forward to the opportunity to bring my skills an
                         </p>
                 </div>
 
-                    <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light 
+                    <div className='col-span-3 relative h-max rounded-2xl border-2 md:border-4 border-solid border-dark bg-light 
                       p-8 dark:bg-dark dark:border-[#b7b7b7] xl:col-span-4 md:order-1 md:col-span-8 md:mb-10'>
 
                     
-                    <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-[#b7b7b7]'/>
+                    <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-[#b7b7b7] md:hidden'/>
 
-                        <Image src={profilePic} alt="Dimitry Roshchin Profile Picture" className='w-full h-auto rounded-2xl'
+                        <Image src={profilePic} alt="Dimitry Roshchin Profile Picture" className='w-full h-auto  rounded-2xl'
                         priority 
                         sizes="(max-width: 760px) 100vw,
                         (max-width: 1200px) 50vw,
