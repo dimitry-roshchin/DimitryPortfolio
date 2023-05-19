69f4b51db56fe6008d84e5a33265fa880204821a
dimitry <dimitry.roshchin@outlook.com>
2023-05-19 12:53:12 -0500

    add themeswitcher icon to navbar in mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -106,6 +106,19 @@ const handleClick = () => {
         </nav>
         
         </div>
+
+        <button 
+              onClick={()=> setMode(mode === "light" ? "dark" : "light")} className={`ml-3 sm:ml-1  w-7 flex flex-wrap items-center justify-center rounded-full p-1
+               ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"} `}>
+                  
+                  
+                  
+                  {
+                    mode === "dark" ?
+                    <SunIcon className={"fill-dark"}/>
+                    : <MoonIcon className={"fill-dark"}/>
+                  }
+              </button> 
       
 
         {
