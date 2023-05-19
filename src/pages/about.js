858d5cd1ebf61a65e04478dfb3d6e4787e103ccc
dimitry <dimitry.roshchin@outlook.com>
2023-05-19 11:13:08 -0500

    image changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import React, { useEffect, useRef } from 'react'
-import profilePic from "../../public/images/profile/jedi.png";
+import profilePic from "../../public/images/profile/avatar.png";
 import Image from 'next/image';
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
 import Skills from '@/components/Skills';
@@ -49,7 +49,7 @@ const about = () => {
     <TransitionEffect/>
         <main className='flex w-full flex-col items-center justify-center dark:text-light'>
             <Layout className='pt-16'>
-            <AnimatedText text="Passion One Must Have" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-10 sm:mt-10'/>
+            <AnimatedText text="Me & My Journey" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-10 sm:mt-10'/>
             <div className='grid w-full grid-cols-8 gap-16 sm:gap-8 '>
                 <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8 md:mb-12 '>
                     <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
@@ -71,7 +71,7 @@ every project I work on. I look forward to the opportunity to bring my skills an
                 </div>
 
                     <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light 
-                     dark:bg-hero p-8 dark:bg-dark dark:border-[#b7b7b7] xl:col-span-4 md:order-1 md:col-span-8 md:mb-10'>
+                      p-8 dark:bg-dark dark:border-[#b7b7b7] xl:col-span-4 md:order-1 md:col-span-8 md:mb-10'>
 
                     
                     <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-[#b7b7b7]'/>
