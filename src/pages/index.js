53f47afdfbd9f692dce36217bf22e7298c720906
dimitry <dimitry.roshchin@outlook.com>
2023-05-21 22:34:07 -0500

    copy update

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,17 +37,17 @@ export default function Home() {
                 <AnimatedText text="Bringing Digital Ideas to Life with Passion and Skill." className='!text-6xl !text-left xl:!text-5xl lg:!text-center
                 lg:!text-6xl md:!text-5xl sm:!text-3xl'/>
                 <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
-                    As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
-                    Explore my latest projects and articles, showcasing my expertise in React.js and web development.
+                Hello, I'm Dimitry, a skilled full-stack developer, my primary goal is to transform ideas into innovative 
+                web applications. You can explore a collection of my recent projects, highlighting my proficiency in React.js and web development.
                 </p>
                 <div className='flex items-center self-start mt-2 lg:self-center'>
-                  <Link href="https://dimitry-portfolio.vercel.app/about" 
+                  <Link href="/about" 
                   className='flex items-center bg-light text-dark p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-[#ff4d70] hover:text-dark
                   border-2 border-solid border-[#ff4d70]  dark:text-light dark:bg-dark dark:hover:bg-[#ff4d70] dark:hover:text-dark md:bg-[#ff4d70]
                   md:p-2 md:px-4 md:text-base sm:bg-[#ff4d70] '
                   
                   >About Dimitry </Link>
-                  <Link href="https://dimitry-portfolio.vercel.app/projects" 
+                  <Link href="/projects" 
                   className='ml-6 text-lg font-medium capitalize text-dark underline underline-offset-2 dark:text-light md:text-base '
                   >Projects</Link>
                 </div>
